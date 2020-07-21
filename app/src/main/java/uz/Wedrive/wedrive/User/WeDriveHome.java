f8a3ebcb38828b21fe43612945914269b0c3e1a8
Og'abek Matyaqubov <OgabekMatyaqubov.2004@gmail.com>
2020-07-21 13:05:21 +0500

    Screenlar qo'shildi, NavigationMenu qo'shildi.

--- app/src/main/java/uz/Wedrive/wedrive/User/WeDriveHome.java
+++ app/src/main/java/uz/Wedrive/wedrive/User/WeDriveHome.java
@@ -8,21 +8,49 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uz.Wedrive.wedrive.R;
+import uz.Wedrive.wedrive.sign.Telephone;
+import uz.Wedrive.wedrive.sign.TelephoneSpinner;
+import uz.Wedrive.wedrive.sign.TelephoneSpinnerAdapter;
 
 public class WeDriveHome extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-
+    Spinner user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.we_drive_home);
 
         bottomNavigationView = findViewById(R.id.bottom_menu);
+        user = findViewById(R.id.username);
+        final List<TelephoneSpinner> customList1 = new ArrayList<>();
+        customList1.add(new TelephoneSpinner(R.drawable.ic_user, "Bog'ibek Matyaqubov"));
+        customList1.add(new TelephoneSpinner(R.drawable.ic_add, "Add new Account"));
+
+        TelephoneSpinnerAdapter telephoneSpinnerAdapter = new TelephoneSpinnerAdapter(WeDriveHome.this, R.layout.spinner_layout, customList1);
+        user.setAdapter(telephoneSpinnerAdapter);
+
+        user.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         bottomNavigationView.setSelectedItemId(R.id.home);
 
