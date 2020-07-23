3c6b641954812e2853175f2a430347895479eada
bogibek <cmditacademy@gmail.com>
2020-07-23 09:51:18 +0500

    xatolik to'grlandi

---
+++ app/src/main/java/uz/Wedrive/wedrive/User/Notification_screen.java
@@ -0,0 +1,16 @@
+package uz.Wedrive.wedrive.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uz.Wedrive.wedrive.R;
+
+public class Notification_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_screen);
+    }
+}
